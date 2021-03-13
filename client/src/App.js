8e22a054737784c4277bc5357c24412dfa15df02
Manav Kushwaha <manavkush@gmail.com>
2021-03-13 22:01:51 +0530

    Did the react Router Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,24 @@
-import './App.css';
-import React, {Fragment} from 'react';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, { Fragment } from "react";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 
-const App = () => {
-  return (
+const App = () => (
+  <Router>
     <Fragment>
-      {/* <h1>App</h1> */}
       <Navbar />
-      <Landing />
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </section>
     </Fragment>
-  );
-}
+  </Router>
+);
 
 export default App;
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
-    return (
-        <section className="landing">
+  return (
+    <section className="landing">
       <div className="dark-overlay">
         <div className="landing-inner">
           <h1 className="x-large">Developer Connector</h1>
@@ -11,12 +12,16 @@ const Landing = () => {
             other developers
           </p>
           <div className="buttons">
-            <a href="register.html" className="btn btn-primary">Sign Up</a>
-            <a href="login.html" className="btn btn-light">Login</a>
+            <Link to="/register" className="btn btn-primary">
+              Sign Up
+            </Link>
+            <Link to="/login" className="btn btn-light">
+              Login
+            </Link>
           </div>
         </div>
       </div>
     </section>
-    )
-}
+  );
+};
 export default Landing;
