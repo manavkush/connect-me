b58999bb5611725f66387dd5ec68ebbde3440e2c
Manav Kushwaha <64668859+manavkush@users.noreply.github.com>
2021-05-30 14:31:20 +0530

    feat: Added CreateProfile Route and form

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import { loadUser } from "./actions/auth";
 import store from "./store";
 import Dashboard from "./components/dashboard/Dashboard";
 import PrivateRoute from "./components/routing/PrivateRoute";
+import CreateProfile from "./components/profile-forms/CreateProfile";
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -35,6 +36,7 @@ const App = () => {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <Route exact path="/create-profile" component={CreateProfile} />
             </Switch>
           </section>
         </Fragment>
