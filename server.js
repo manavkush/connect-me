160e4f8854da2790f08f91141454253754fc57e2
Manav Kushwaha <64668859+manavkush@users.noreply.github.com>
2021-06-03 19:34:52 +0530

    Prepared for deployment

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const { connect } = require("mongoose");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
 // Connect Database
 connectDB();
-
-app.get("/", (req, res) => {
-  res.send("API running");
-});
 
 // Init Middleware
 // Enables us to get the body of the request
@@ -20,6 +17,14 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
